be67c72a6a7796369021c5b31712c2dd24f991b5
dev <remotedeveloper88@gmail.com>
2016-12-18 21:30:19 -0200

    Finished the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
             presence: true,
             length: { minimum: 3, maximum: 255 },
             uniqueness: { case_sensitive: false }
-  validates :password, presence: true
+
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  # digest from plain '123456'
+  PASSWORD = '$2a$10$rCnzFVQE9aJ7cn45iH63LO7gm8ML.kIwfay8/s1ZLd7SCREI/IRn6'
+             .freeze
+
   def setup
-    @user = User.new(name: 'Monica', password: '123456', access: 0)
+    @user = User.new(
+      name: 'Monica',
+      password: '123456',
+      password_confirmation: '123456',
+      access: 0
+    )
   end
 
   test 'should be valid' do
@@ -37,5 +46,15 @@ class UserTest < ActiveSupport::TestCase
   test 'password should be present' do
     @user.password = nil
     assert_not @user.valid?
+  end
+
+  test 'password confirmation should be valid' do
+    @user.password_confirmation = '1' * 6
+    assert_not @user.valid?
+  end
+
+  test 'should authenticate' do
+    @user.save
+    assert @user.authenticate('123456')
   end
 end
